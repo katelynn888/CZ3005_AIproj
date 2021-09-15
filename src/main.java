56c3bf8e817f19c8c79d6885276a40a2c2a26bac
josephinehemingway <josephine.hemingway@gmail.com>
2021-09-15 18:04:35 +0800

    print node connected

--- src/main.java
+++ src/main.java
@@ -1,4 +1,40 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class main {
+	public static void main(String args[]) throws IOException {
 
+		// reading map network file & creating a graph
+		String filepath = "G.json";
+		JSONParser parser = new JSONParser();
+		
+		try {     
+            Object obj = parser.parse(new FileReader(filepath));
+            
+            JSONObject jsonObj = (JSONObject) obj;
+            
+            for (int i=1; i<=5; i++) {
+            	String node = String.format("%d", i);
+            	JSONArray One = (JSONArray) jsonObj.get(node);
+                System.out.println(One);
+            }
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+		
+	}
 }
